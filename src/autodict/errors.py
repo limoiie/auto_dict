fd0b6861112a1652c8c6774d4fc5a59b4d61f412
limoiie <limo.iie4@gmail.com>
2022-12-05 23:28:26 +0800

    refactor: Adjust error structure

--- src/autodict/errors.py
+++ src/autodict/errors.py
@@ -1,13 +1,21 @@
 from typing import Optional
 
 
-class UnableToDict(Exception):
+class AutoDictError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class UnableToDict(AutoDictError):
     def __init__(self, cls: Optional[type]):
         super().__init__(
             f'{cls}, please mark it as to_dictable.')
 
 
-class UnableFromDict(Exception):
+class UnableFromDict(AutoDictError):
     def __init__(self, cls: Optional[type]):
         super().__init__(
             f'{cls}, please mark it as from_dictable.')
